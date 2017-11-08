74b9fcecc12fbb0b6a596197b8468a3ff7556805
lturpin <laurent.turpin@insa-lyon.fr>
2017-11-08 11:02:15 +0100

    Non fonctionel mais first

---
+++ app/src/main/java/com/insa/lsbdp/ihm/ListSport.java
@@ -0,0 +1,42 @@
+package com.insa.lsbdp.ihm;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ListSport extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.choix_sport:
+                    mTextMessage.setText(R.string.title_choix);
+                    return true;
+                case R.id.voeux_sport:
+                    mTextMessage.setText(R.string.title_voeux);
+                    return true;
+                default :
+                    return false;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_sport);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
